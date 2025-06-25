ae2d538f720d15045ea7b8c2d7072d4010b28e95
Resyourbae <resyaanggara98@gmail.com>
2025-06-25 20:31:46 +0700

    Resya Istri Ibuki: Mengedit di bagian fortify.php yaitu mengaktifkan bagian Features::emailVerification(), lalu di web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/dev', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified',
+    // 'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
